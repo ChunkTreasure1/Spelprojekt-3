90aed0844634d9a08a3eb1900994509726e065dd
Samuelharenhest <48718865+Samuelharenhest@users.noreply.github.com>
2019-03-29 10:56:59 +0100

    Update Sam

---
+++ Assets/LoadScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour
+{
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+        Time.timeScale = 1f;
+    }
+
+}
