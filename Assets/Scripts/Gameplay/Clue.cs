4ad8677a15180588ec45dfb09dae7c0e2f9b31f5
Ivar Jönsson <ivar.jonsson@shadowgamestudio.net>
2019-04-11 21:24:50 +0200

    Update

---
+++ Assets/Scripts/Gameplay/Clue.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clue : MonoBehaviour
+{
+    [SerializeField] private string TextToShow;
+    [SerializeField] private BoxCollider Area;
+
+    private GameObject Text;
+    private bool Entered = false;
+
+    void Start()
+    {
+        Text = GameObject.FindGameObjectWithTag("Text");
+    }
+
+    void Update()
+    {
+        if (Entered)
+        {
+            if (Input.GetKeyDown(KeyCode.F))
+            {
+                Text.GetComponent<Text>().text = TextToShow;
+            }
+        }
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            //Show interact text
+            Entered = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.CompareTag("Player"))
+        {
+            Entered = false;
+        }
+    }
+}
