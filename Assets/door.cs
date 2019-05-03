e1975dcef8a934a48eee1020834248c8ada3241b
plsGive <48825206+plsGive@users.noreply.github.com>
2019-05-03 10:43:27 +0200

    Update notes
    Notes and door/key

---
+++ Assets/door.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class door : MonoBehaviour
+{
+    public string myString;
+    public string openString;
+    public Text myText;
+    public float fadeTime;
+    public bool displayInfo;
+    private key myScript;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //myText = GameObject.Find("").GetComponent<Text>();
+        myText.color = Color.clear;
+        myScript = GetComponent<key>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FadeText();
+    }
+
+    private void OnMouseOver()
+    {
+
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+               displayInfo = true;
+            }
+    }
+    private void OnMouseExit()
+    {
+        displayInfo = false;
+    }
+    void FadeText()
+    {
+        if (displayInfo)
+        {
+            if (myScript.keyPickedUp==true)
+            {
+                myText.text = openString;
+            }
+            else
+            {
+                myText.text = myString;
+            }
+
+            myText.color = Color.Lerp(myText.color, Color.white, fadeTime * Time.deltaTime);
+        }
+        else
+        {
+            myText.color = Color.Lerp(myText.color, Color.clear, fadeTime * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/key.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour
+{
+    public GameObject keyUI;
+    public bool keyPickedUp = false;
+    private displayE myScript;
+    public void Start()
+    {
+        myScript = GetComponentInChildren<displayE>();
+    }
+    public void Update()
+    {
+        if (myScript.displayInfo == true)
+        {
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                 pickUp();
+            }
+        }
+    }
+    public void pickUp()
+    {
+        keyPickedUp = true;
+        keyUI.SetActive(false);
+    }
+}
