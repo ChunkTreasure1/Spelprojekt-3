ba159d396a7faf93a71d7c433f05ef116e296d9c
Tompa <45692040+ThomasB02@users.noreply.github.com>
2019-06-01 18:03:20 +0200

    klar

--- Assets/MianMenu.cs
+++ Assets/MianMenu.cs
@@ -9,4 +9,8 @@ public class MianMenu : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
 }
